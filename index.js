a2800ac1a31a404df43c33e5d01e3251370c021a
Frank Ackermann <acki77.github@digital-filestore.de>
2026-01-05 14:29:26 +0100

    bookmark button

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 // phew… not a lot going on here. Please add some code!
+// Bookmark Toggle
+const bookmarkButton = document.querySelector('[data-js="bookmark-active"]');
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
+
+// Answer Toggle
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answerText = document.querySelector('[data-js="answer-text"]');
+
+answerButton.addEventListener("click", () => {
+  answerText.classList.toggle("card__answer--active");
+
+  // Button Text anpassen
+  if (answerText.classList.contains("card__answer--active")) {
+    answerButton.textContent = "Hide answer";
+  } else {
+    answerButton.textContent = "Show answer";
+  }
+});
 const bookmarkButton = document.querySelector('[data-js="bookmark-active"]');
 
 bookmarkButton.addEventListener("click", () => {
-  bookmarkButton.classList.toggle("bookmark-active");
+  bookmarkButton.classList.toggle("bookmark--active");
 });
